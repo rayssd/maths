e7705850a2b6d19976c571a3784f5b1dc98c0deb
Raymond Hu <rayssd@hotmail.com>
2025-06-19 11:59:50 +1000

    added more simple polynomials

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,36 @@ struct Question {
     question_string: String,
 }
 
+impl Question {
+    fn new_addition_and_subtraction(range: i32) -> Self {
+        let lhs = rand::thread_rng().gen_range(10..=range);
+        let rhs = rand::thread_rng().gen_range(0..=range);
+        let result = 0;
+        let question_string = String::new();
+
+        Question {
+            lhs,
+            rhs,
+            result,
+            question_string,
+        }
+    }
+
+    fn new_multiplication_and_division(range: i32) -> Self {
+        let lhs = rand::thread_rng().gen_range(0..=range);
+        let rhs = rand::thread_rng().gen_range(2..=range);
+        let result = 0;
+        let question_string = String::new();
+
+        Question {
+            lhs,
+            rhs,
+            result,
+            question_string,
+        }
+    }
+}
+
 struct SimplePolynomial {
     n1: i32,
     n2: i32,
@@ -22,15 +52,28 @@ struct SimplePolynomial {
     question_string: String,
 }
 
+impl SimplePolynomial {
+    fn new(range: i32) -> Self {
+        let n1 = rand::thread_rng().gen_range(1..=range);
+        let n2 = rand::thread_rng().gen_range(1..=range);
+        let n3 = rand::thread_rng().gen_range(1..=range);
+        let n4 = rand::thread_rng().gen_range(1..=range);
+        let result = 0;
+        let question_string = String::new();
+
+        SimplePolynomial {
+            n1,
+            n2,
+            n3,
+            n4,
+            result,
+            question_string,
+        }
+    }
+}
+
 fn simple_algebra(range: i32) -> SimplePolynomial {
-    let mut my_question = SimplePolynomial {
-        n1: rand::thread_rng().gen_range(1..=range),
-        n2: rand::thread_rng().gen_range(1..=range),
-        n3: 0,
-        n4: 0,
-        result: 0,
-        question_string: String::new(),
-    };
+    let mut my_question = SimplePolynomial::new(range);
 
     my_question.n3 = my_question.n1 + my_question.n2;
     (my_question.question_string, my_question.result) =
@@ -56,31 +99,67 @@ fn simple_algebra(range: i32) -> SimplePolynomial {
 
     my_question
 }
-
-fn simple_polynomial(range: i32) -> SimplePolynomial {
-    let mut my_question = SimplePolynomial {
-        n1: rand::thread_rng().gen_range(1..=range),
-        n2: rand::thread_rng().gen_range(1..=range),
-        n3: rand::thread_rng().gen_range(1..=range),
-        n4: rand::thread_rng().gen_range(1..=range),
-        result: 0,
-        question_string: String::new(),
-    };
+fn simple_polynomial1(range: i32) -> SimplePolynomial {
+    // x + {} + {} = {} + {} & {} -x + {} = {} + {} and variations
+    let mut my_question = SimplePolynomial::new(range);
 
     my_question.result = my_question.n4 + my_question.n3 - my_question.n2 - my_question.n1;
 
+    let seed = rand::thread_rng().gen_range(1..=5);
+
     match my_question.result.cmp(&0) {
         Ordering::Greater => {
-            my_question.question_string = format!(
-                "{} + x + {} = {} + {}\nWhat's x? ",
-                my_question.n1, my_question.n2, my_question.n3, my_question.n4
-            );
+            my_question.question_string = match seed {
+                1 => format!(
+                    "{} + x + {} = {} + {}\nWhat's x? ",
+                    my_question.n1, my_question.n2, my_question.n3, my_question.n4
+                ),
+
+                2 => format!(
+                    "x + {} + {} = {} + {}\nWhat's x? ",
+                    my_question.n1, my_question.n2, my_question.n3, my_question.n4
+                ),
+
+                3 => format!(
+                    "{} + {} + x = {} + {}\nWhat's x? ",
+                    my_question.n1, my_question.n2, my_question.n3, my_question.n4
+                ),
+
+                4 => format!(
+                    "{} + {} = {} + x + {}\nWhat's x? ",
+                    my_question.n3, my_question.n4, my_question.n1, my_question.n2
+                ),
+
+                _ => format!(
+                    "{} + {} = x + {} + {}\nWhat's x? ",
+                    my_question.n3, my_question.n4, my_question.n1, my_question.n2
+                ),
+            };
         }
+
         _ => {
-            my_question.question_string = format!(
-                "{} - x + {} = {} + {}\nWhat's x? ",
-                my_question.n1, my_question.n2, my_question.n3, my_question.n4
-            );
+            my_question.question_string = match seed {
+                1 => format!(
+                    "{} - x + {} = {} + {}\nWhat's x? ",
+                    my_question.n1, my_question.n2, my_question.n3, my_question.n4
+                ),
+
+                2 => format!(
+                    "{} + {} - x = {} + {}\nWhat's x? ",
+                    my_question.n1, my_question.n2, my_question.n3, my_question.n4
+                ),
+
+                3 => format!(
+                    "{} + {} = {} - x + {}\nWhat's x? ",
+                    my_question.n3, my_question.n4, my_question.n1, my_question.n2
+                ),
+
+                _ => format!(
+                    "{} + {} = {} + {} - x\nWhat's x? ",
+                    my_question.n3, my_question.n4, my_question.n1, my_question.n2
+                ),
+            };
+
             my_question.result = my_question.result.abs();
         }
     };
@@ -88,13 +167,58 @@ fn simple_polynomial(range: i32) -> SimplePolynomial {
     my_question
 }
 
-fn addition(range: i32) -> Question {
-    let mut my_question = Question {
-        lhs: rand::thread_rng().gen_range(10..=range),
-        rhs: rand::thread_rng().gen_range(0..=range),
-        result: 0,
-        question_string: String::new(),
+fn simple_polynomial2(range: i32) -> SimplePolynomial {
+    // x - {} - {} = {} + {} and variations
+
+    let mut my_question = SimplePolynomial::new(range);
+
+    my_question.result = my_question.n4 + my_question.n3 + my_question.n2 + my_question.n1;
+
+    let seed = rand::thread_rng().gen_range(0..=1);
+
+    my_question.question_string = match seed {
+        0 => format!(
+            "x - {} - {} = {} + {}\nWhat's x? ",
+            my_question.n1, my_question.n2, my_question.n3, my_question.n4
+        ),
+
+        _ => format!(
+            "{} + {} = x - {} - {}\nWhat's x? ",
+            my_question.n3, my_question.n4, my_question.n1, my_question.n2
+        ),
     };
+
+    my_question
+}
+
+fn simple_polynomial3(range: i32) -> SimplePolynomial {
+    // x - {} + {} = {} + {} and variations
+
+    let mut my_question = SimplePolynomial::new(range);
+
+    let max_n2 = my_question.n1 + my_question.n3 + my_question.n4;
+    my_question.n2 = rand::thread_rng().gen_range(1..=max_n2);
+
+    my_question.result = my_question.n4 + my_question.n3 - my_question.n2 + my_question.n1;
+
+    let seed = rand::thread_rng().gen_range(0..=1);
+
+    my_question.question_string = match seed {
+        0 => format!(
+            "x - {} + {} = {} + {}\nWhat's x? ",
+            my_question.n1, my_question.n2, my_question.n3, my_question.n4
+        ),
+
+        _ => format!(
+            "{} + {} = x - {} + {}\nWhat's x? ",
+            my_question.n3, my_question.n4, my_question.n1, my_question.n2
+        ),
+    };
+
+    my_question
+}
+fn addition(range: i32) -> Question {
+    let mut my_question = Question::new_addition_and_subtraction(range);
 
     my_question.result = my_question.lhs + my_question.rhs;
     my_question.question_string = format!("{} + {} = ", my_question.lhs, my_question.rhs);
@@ -103,12 +227,7 @@ fn addition(range: i32) -> Question {
 }
 
 fn subtraction(range: i32) -> Question {
-    let mut my_question = Question {
-        lhs: rand::thread_rng().gen_range(10..=range),
-        rhs: rand::thread_rng().gen_range(0..=range),
-        result: 0,
-        question_string: String::new(),
-    };
+    let mut my_question = Question::new_addition_and_subtraction(range);
 
     if my_question.rhs > my_question.lhs {
         std::mem::swap(&mut my_question.lhs, &mut my_question.rhs);
@@ -121,12 +240,7 @@ fn subtraction(range: i32) -> Question {
 }
 
 fn multiplication(range: i32) -> Question {
-    let mut my_question = Question {
-        lhs: rand::thread_rng().gen_range(0..=range),
-        rhs: rand::thread_rng().gen_range(2..=range),
-        result: 0,
-        question_string: String::new(),
-    };
+    let mut my_question = Question::new_multiplication_and_division(range);
 
     my_question.result = my_question.lhs * my_question.rhs;
     my_question.question_string = format!("{} x {} = ", my_question.lhs, my_question.rhs);
@@ -135,12 +249,7 @@ fn multiplication(range: i32) -> Question {
 }
 
 fn division(range: i32) -> Question {
-    let mut my_question = Question {
-        lhs: rand::thread_rng().gen_range(0..=range),
-        rhs: rand::thread_rng().gen_range(2..=range),
-        result: 0,
-        question_string: String::new(),
-    };
+    let mut my_question = Question::new_multiplication_and_division(range);
 
     my_question.result = my_question.lhs;
     my_question.lhs = my_question.lhs * my_question.rhs;
@@ -150,13 +259,15 @@ fn division(range: i32) -> Question {
 }
 
 fn main() {
+    let name = "Peach";
+
     println!(
         "{}",
         "*********************************************".purple()
     );
     println!(
         "Hi {}, Welcome to daddy's maths challenge!",
-        "Elise".bright_magenta()
+        name.bright_magenta()
     );
     println!(
         "{}",
@@ -186,8 +297,14 @@ fn main() {
         //     _ => break,
         // };
 
-        // let question = simple_polynomial(simple_polynomial_range);
-        let question = simple_algebra(simple_algebra_range);
+        let question = match Uniform::from(0..=2).sample(&mut rand::thread_rng()) {
+            0 => simple_polynomial1(simple_polynomial_range),
+            1 => simple_polynomial2(simple_polynomial_range),
+            2 => simple_polynomial3(simple_polynomial_range),
+            _ => break,
+        };
+
+        //let question = simple_algebra(simple_algebra_range);
 
         println!("{}", "***********".yellow());
         println!("{} {}", "Question".blue(), question_number);
@@ -209,7 +326,7 @@ fn main() {
                 match answer.trim().parse() {
                     Ok(num) => break num,
                     Err(_) => {
-                        println!("Elise, that's not a valid number!");
+                        println!("{}, that's not a valid number!", name);
                         println!();
                         print!("{}", question.question_string);
                         io::stdout().flush().unwrap();
@@ -220,7 +337,7 @@ fn main() {
 
             match answer.cmp(&question.result) {
                 Ordering::Equal => {
-                    println!("Well done, {}!", "Elise".bright_magenta());
+                    println!("Well done, {}!", name.bright_magenta());
                     println!();
                     is_answer_correct = true;
                 }
@@ -243,19 +360,19 @@ fn main() {
 
         if minutes == 1 {
             println!(
-                "Great work, Elise! You've completed {} challenges in {} minute {} seconds!",
-                total_questions, minutes, seconds
+                "Great work, {}! You've completed {} challenges in {} minute {} seconds!",
+                name, total_questions, minutes, seconds
             );
         } else {
             println!(
-                "Great work, Elise! You've completed {} challenges in {} minutes {} seconds!",
-                total_questions, minutes, seconds
+                "Great work, {}! You've completed {} challenges in {} minutes {} seconds!",
+                name, total_questions, minutes, seconds
             );
         }
     } else {
         println!(
-            "Great work, Elise! You've completed {} challenges in {} seconds!",
-            total_questions, elapsed_secs
+            "Great work, {}! You've completed {} challenges in {} seconds!",
+            name, total_questions, elapsed_secs
         );
     }
 }
